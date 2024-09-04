ec3ebbb746c8336fa3ddecf3e0f1c0e6636f3ff3
Marco.S <133777873+marco1cas@users.noreply.github.com>
2024-09-04 16:36:25 -0400

    Update NetOptimizer.bat

--- windows/cmd/projects/NetOptimizer.bat
+++ windows/cmd/projects/NetOptimizer.bat
@@ -1,1 +1,32 @@
+@echo off 
+
+echo //////////////////////////////
+echo //// Solve Network Problems ////
+echo //////////////////////////////
+
+start cmd /k "ipconfig /all"
+
+echo Limpiando cache de DNS...
+ipconfig /flushdns
+echo.
+
+echo Actualizando el registro de DNS...
+ipconfig /registerdns
+echo.
+
+echo Liberando tu direccion IP...
+ipconfig /release
+echo.
+
+echo Renovando una nueva IP...
+ipconfig /renew
+echo.
+
+echo Reiniciando Winsock para resolver problemas de conexion...
+netsh winsock reset
+echo.
+
+echo ./././././.Finalizado./././././././
+pause
+exit
 
