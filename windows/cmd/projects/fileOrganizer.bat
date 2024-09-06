5603f2a9df069f65ac3572a70c59375266d16c6b
Marco.S <133777873+marco1cas@users.noreply.github.com>
2024-09-06 16:33:42 -0400

    Update fileOrganizer.bat

--- windows/cmd/projects/fileOrganizer.bat
+++ windows/cmd/projects/fileOrganizer.bat
@@ -1,1 +1,27 @@
+@echo off
+
+:: expancion retardada de variables dentro de bucles
+setlocal enabledelayedexpansion
+:: %~dp0 obtiene la ruta actual de donde se ejecute el bat
+set "fileExtend=%~dp0" 
+
+mkdir "%fileExtend%docx"
+mkdir "%fileExtend%pptx"
+mkdir "%fileExtend%jpg"
+mkdir "%fileExtend%xlsx"
+mkdir "%fileExtend%pdf"
+mkdir "%fileExtend%exe" 
+:: (*.*) significa todos los archivos de la ruta
+::  %%f obtiene la extension del archivo actual 
+::  ~x extrae solo la extensión del archivo
+:: !varBat! hace referencia a la expancion retardada
+for %%f in (*.*) do (
+    set "varBat=%%~xf"
+    if /i "!varBat!" == ".docx" move "%%f" "%fileExtend%docx\"
+    if /i "!varBat!" == ".pptx" move "%%f" "%fileExtend%pptx\"
+    if /i "!varBat!" == ".jpg" move "%%f" "%fileExtend%jpg\"
+    if /i "!varBat!" == ".xlsx" move "%%f" "%fileExtend%xlsx\"
+    if /i "!varBat!" == ".pdf" move "%%f" "%fileExtend%pdf\"
+    if /i "!varBat!" == ".exe" move "%%f" "%fileExtend%exe\"
+)
 
