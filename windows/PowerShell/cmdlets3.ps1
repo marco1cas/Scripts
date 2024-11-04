7e7d94ad766632ffdcb3a902fdc34d9a1091b511
Marco.S <133777873+marco1cas@users.noreply.github.com>
2024-11-04 17:39:38 -0400

    Add files via upload

---
+++ windows/PowerShell/cmdlets3.ps1
@@ -0,0 +1,30 @@
+﻿<#
+Comandos unicos de una linea y canalizacion
+
+-Comandos Unicos de una linea son los que se ejecutan directamente en la terminal (get-help, get-process, get-service)
+para saber mas utiliza Get-Command
+
+
+-La canalización (|) es una técnica que te permite pasar la salida de un cmdlet como entrada de otro cmdlet
+
+Get-Process | Sort-Object CPU -Descending <-- ordenar de forma decendente los procesos que gasten mas CPU
+(get-process y sort-object son cmdlets, CPU es una propiedad de get-process y -descending parametro de sort-object)
+
+
+-Scritp Block ejecuta operaciones dentro de la canalizacion usando llaves actua como un filtro avanzado 
+
+
+Get-Service | Where-Object {$_.Status -eq 'Running'} <-- Obtenemos todos los servicios que esten running
+
+(Get-Service y Where-Object(seleciona objetos basandose en sus valores de propiedad) son cmdlets, {$_.Status} {} bloque 
+$_ representa servicios individualmente, Status propiedad de Get-Service, -eq es un parametro de Where-Object  indica si el 
+valor "Running" es true y los mostrara )
+
+
+(Ejemplos Script Block)
+Get-Process | Where-Object {$_.CPU -gt 90} | Select-Object Name, CPU <-- filtra procesos con uso de cpu mayor a 90
+
+Get-Service | Where-Object {$_.Status -eq 'Running'} <-- muestra los servicios que estan en running
+
+
+#> 
---
+++ windows/PowerShell/cmdlets4.ps1
@@ -0,0 +1,43 @@
+﻿<# 
+Formato, Alias, Proveedores, Comparacion 
+
+-Formato (formatear) la salida para obtener una presentacion mas legible
+
+Get-Process | Sort-Object CPU -Descending | Format-Table Name, CPU -AutoSize <-- formatea como una tabla despues de haber ordenado los procesos 
+por uso de CPU. (Format-Table <es un cmdlet> formatea datos en forma de tabla, -AutoSize Ajusta el ancho de las columnas ) 
+
+Get-Service | Sort-Object Status, DisplayName | Format-Table DisplayName, Status, StartType -AutoSize <-- mostrar la información de los servicios con Get-Service ajustando las columnas para una presentación ordenada
+
+-Alias  nombres abreviados para comandos largos o cmdlets
+
+gcm == Get-Command
+ps == Get-process
+gsv == get-service 
+gm == Get-Member
+gal == Get-Alias
+ft == Format-Table
+
+ps | Sort-Object PM -Descending | ft Name, CPU, PM -AutoSize
+
+
+-Proveedores  permiten acceder a diferentes tipos de datos como si fueran sistemas de archivos
+
+Get-PSProvider (obtiene los proveedores de PowerShell en la sesión actual. Se puede obtener una determinada unidad de disco o todas las unidades de la sesión.)
+
+set-location C:\\Users\Usuario\Documents <-- Cmabia a la unidad de archivos 
+
+dir HKCU:\\Software <-- Explora el registro de Windows
+
+
+-Operadores de Comparacion
+
+5 -eq 5 (igual a)
+2 -ne 1 (no igual a)
+10 -gt 9 (Mayor que)
+2 -lt 10 (Menor que)
+'Powershell' -like 'Power*' (Coincidencia con comodines)
+#>
+
+
+
+
