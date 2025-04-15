874e51071cbaada4dba86e438bf3fb574128a541
Marco.S <133777873+marco1cas@users.noreply.github.com>
2025-04-15 16:23:52 -0400

    Add files via upload
    Automatic File Organizer

---
+++ windows/PowerShell/Projects/Organizer.ps1
@@ -0,0 +1,14 @@
+﻿# Automatic file organizer
+
+$file = "C:\users\usuario17\Documents\prove\file2"
+
+Get-ChildItem -Path $file -File | ForEach-Object {
+    $extension = $_.Extension.TrimStart(".")
+    $subFolder = Join-Path -Path $file -ChildPath $extension
+
+    if (-not (Test-Path -Path $subFolder)) {
+        New-Item -Path $subFolder -ItemType Directory | Out-Null
+    }
+    Move-Item -Path $_.FullName -Destination $subFolder
+}
+
